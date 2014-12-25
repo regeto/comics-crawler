5d6091f660d4c0fe42e634ee6de253321276e41f
regeto <regendokira@gmail.com>
2014-12-25 10:27:12 +0100

    added option to download a whole chapter at once

--- crawler.py
+++ crawler.py
@@ -6,6 +6,7 @@ class Crawler:
     site = ""
     url = ""
     search = ""
+    illegal_characters = ['<', '>', ':', '"', '/', '\\', '|', '?', '*']
 
     def search_series(self, title):
         # Search for comic by its name, then return a list of matches
@@ -29,3 +30,22 @@ class Crawler:
         if not os.path.exists(path):
             os.makedirs(path)
         urllib.request.urlretrieve(image_url, path + file + "." + extension)
+
+    def get_chapter_name(self, name):
+        return ''.join([char for char in name if char not in self.illegal_characters])
+
+    def save_chapter(self, chapter_name, image_urls, file_names, path=""):
+        # Save all images in a list to the given path
+        chapter = self.get_chapter_name(chapter_name)
+        file_path = path + chapter + "/"
+        if os.path.exists(file_path):
+            print("The folder at \"" + file_path + "\" already exists. This chapter has been skipped.")
+            return False
+        if len(image_urls) != len(file_names):
+            print("The url and name lists don't seem to fit together! This chapter has been skipped.")
+            return False
+        os.makedirs(file_path)
+        for i in range(len(image_urls)):
+            print("Retrieving image " + file_names[i] + "...")
+            urllib.request.urlretrieve(image_urls[i], file_path + file_names[i])
+        return True
