b8817e7d4efb6785326e1afc67c78b9e767076c1
regeto <regendokira@gmail.com>
2014-12-29 19:14:31 +0100

    added support for KissManga

--- crawlers/__init__.py
+++ crawlers/__init__.py
@@ -3,6 +3,7 @@ from crawlers.bato_crawler import BatoCrawler
 from crawlers.dynasty_crawler import DynastyCrawler
 from crawlers.eden_crawler import EdenCrawler
 from crawlers.kawaii_crawler import KawaiiCrawler
+from crawlers.kiss_crawler import KissCrawler
 from crawlers.oots_crawler import OotsCrawler
 from crawlers.smackjeeves_crawler import SmackjeevesCrawler
 
@@ -11,6 +12,7 @@ __all__ = [
     'DynastyCrawler',
     'EdenCrawler',
     'KawaiiCrawler',
+    'KissCrawler',
     'OotsCrawler',
     'SmackjeevesCrawler',
 ]
---
+++ crawlers/kiss_crawler.py
@@ -0,0 +1,31 @@
+import re
+from crawlers import Crawler
+
+class KissCrawler(Crawler):
+    site = "Kissmanga"
+    url = "http://kissmanga.com"
+
+    def get_chapters(self, series_url):
+        html = self.get_html(series_url)
+        regex = "<td>\n<a href=\"(.*?)\".*?>\n(.*?)<\/a>\n<\/td>"
+        reg = re.compile(regex)
+        r = reg.findall(html)
+        r.reverse()
+        ret = [None] * len(r)
+        pos = 0
+        for x in r:
+            ret[pos] = dict(url=self.url + x[0], name=self.get_complete_file_name(x[0]))
+            pos += 1
+        return ret
+
+    def get_pages(self, chapter_url):
+        html = self.get_html(chapter_url)
+        regex = "lstImages.push\(\"(.*?)\"\);"
+        reg = re.compile(regex)
+        r = reg.findall(html)
+        ret = [None] * len(r)
+        pos = 0
+        for x in r:
+            ret[pos] = dict(url=x, name = self.get_complete_file_name(x))
+            pos += 1
+        return ret
