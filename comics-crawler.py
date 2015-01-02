925bb2501f2fc02506195e8190d15b01746d22f2
regeto <regendokira@gmail.com>
2015-01-03 00:03:35 +0100

    split updating into own module

--- comics-crawler.py
+++ comics-crawler.py
@@ -1,53 +1,5 @@
-from crawlers import *
-from sources import *
 import argparse
-
-# directory prefix
-# use this if the sources only list directories relative to some sort of comic folder
-directory = ""
-
-
-def update_batoto():
-    do_update(BatoCrawler(), batoto.sources)
-
-
-def update_dynasty():
-    do_update(DynastyCrawler(), dynasty.sources)
-
-
-def update_eden():
-    do_update(EdenCrawler(), eden.sources)
-
-
-def update_kawaii():
-    do_update(KawaiiCrawler(), kawaii.sources)
-
-
-def update_oots():
-    do_update(OotsCrawler(), oots.sources)
-
-
-def update_smackjeeves():
-    do_update(SmackjeevesCrawler(), smackjeeves.sources)
-
-
-def do_update(crawler, slist):
-    for series in slist:
-        crawler.download_series(series['url'],
-                                directory + series['dir'],
-                                force='force' in series,
-                                oneshot='oneshot' in series,
-                                webcomic='webcomic' in series,
-                                )
-
-
-def update_all():
-    update_batoto()
-    update_dynasty()
-    update_eden()
-    update_kawaii()
-    update_oots()
-    update_smackjeeves()
+import updater
 
 
 def launch_command_line_interface():
@@ -60,7 +12,7 @@ parser.add_argument("--update", help="update from all sites after everything els
 args = parser.parse_args()
 
 if args.update:
-    update_all()
+    updater.update_all()
     exit()
 if not any(vars(args).values()):
     launch_command_line_interface()
---
+++ updater.py
@@ -0,0 +1,49 @@
+from crawlers import *
+from sources import *
+
+# directory prefix
+# use this if the sources only list directories relative to some sort of comic folder
+directory = ""
+
+
+def update_batoto():
+    do_update(BatoCrawler(), batoto.sources)
+
+
+def update_dynasty():
+    do_update(DynastyCrawler(), dynasty.sources)
+
+
+def update_eden():
+    do_update(EdenCrawler(), eden.sources)
+
+
+def update_kawaii():
+    do_update(KawaiiCrawler(), kawaii.sources)
+
+
+def update_oots():
+    do_update(OotsCrawler(), oots.sources)
+
+
+def update_smackjeeves():
+    do_update(SmackjeevesCrawler(), smackjeeves.sources)
+
+
+def update_all():
+    update_batoto()
+    update_dynasty()
+    update_eden()
+    update_kawaii()
+    update_oots()
+    update_smackjeeves()
+
+
+def do_update(crawler, slist):
+    for series in slist:
+        crawler.download_series(series['url'],
+                                directory + series['dir'],
+                                force='force' in series,
+                                oneshot='oneshot' in series,
+                                webcomic='webcomic' in series,
+                                )
