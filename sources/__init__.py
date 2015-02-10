1e4ad84c02ab923f6d1094bedc39219b8ece5a45
regeto <regendokira@gmail.com>
2015-02-10 16:13:05 +0100

    extending on previous commit

--- sources/__init__.py
+++ sources/__init__.py
@@ -2,6 +2,7 @@ from sources import bato_sources as batoto
 from sources import dynasty_sources as dynasty
 from sources import eden_sources as eden
 from sources import kawaii_sources as kawaii
+from sources import kiss_sources as kiss
 from sources import oots_sources as oots
 from sources import smackjeeves_sources as smackjeeves
 
