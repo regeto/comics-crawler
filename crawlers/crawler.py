regeto/comics-crawler

commit 515eac3f8d1058e092cd384d40556a5d356014c8

fixed a bug where glob.glob would not find folders with square brackets. Also streamlined character replacement

crawlers/crawler.py
@@ -9,8 +9,20 @@ class Crawler:
     site = ""
     url = ""
     search = ""
-    illegal_characters = ['<', '>', ':', '"', '/', '\\', '|', '?', '*']
-    replacesd_characters = [('\t', ' ')]
+    replaced_characters = {
+                        '<': '',
+                        '>': '',
+                        ':': '',
+                        '"': '',
+                        '/': '',
+                        '\\': '',
+                        '|': '',
+                        '?': '',
+                        '*': '',
+                        '\t': ' ',
+                        '[': '[[]',
+                        ']': '[]]'
+    }
     verbose = False
 
     def search_series(self, title):
@@ -43,9 +55,10 @@ class Crawler:
         return gf.read().decode("utf-8", 'ignore')
 
     def get_name_for_file_system(self, name):
-        for x, y in self.replacesd_characters:
-            name = name.replace(x, y)
-        name = (''.join([char for char in name if char not in self.illegal_characters])).strip()
+        name = (''.join([char if char not in self.replaced_characters
+                         else self.replaced_characters[char]
+                         for char in name])
+                ).strip()
         while (name[-1] == '.') or (name[-1] == ' '):
             name = name[:-1]
         return name

commit 638842856071858c49a92141b0e00046429c2b4e

fixed stupid bug introduced in 515eac3f8d1058e092cd384d40556a5d356014c8, use os.path.exists instead of glob

crawlers/crawler.py
@@ -20,8 +20,6 @@ class Crawler:
                         '?': '',
                         '*': '',
                         '\t': ' ',
-                        '[': '[[]',
-                        ']': '[]]'
     }
     verbose = False
 
@@ -129,7 +127,7 @@ class Crawler:
             chapter = chapters[i]
             name = self.get_name_for_file_system(chapter['name'])
             cpath = path + name
-            if glob.glob(cpath):
+            if os.path.exists(cpath):
                 continue
             pages = self.get_pages(chapter['url'])
             update = dict(